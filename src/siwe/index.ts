2c25fe1e61ec5aaa0b0f0d16f3f2ef0cab96d2af
Adeiga Oluwadamilare <grexx200@gmail.com>
2026-07-22 12:22:09 +0100

    feat: add SIWE replay protection

--- src/siwe/index.ts
+++ src/siwe/index.ts
@@ -3,5 +3,22 @@
  *
  * @module siwe
  */
-export type { SiweMessage, SiweVerifyParams, SiweVerifyResult, SiweParseResult } from './siwe.types';
-export { formatSiweMessage, parseSiweMessage, verifySiweSignature, generateSiweNonce, MAX_SIWE_MESSAGE_LENGTH } from './siwe.helpers';
+export type {
+  SiweMessage,
+  SiweVerifyParams,
+  SiweVerifyResult,
+  SiweParseResult,
+} from './siwe.types';
+
+export {
+  formatSiweMessage,
+  parseSiweMessage,
+  verifySiweSignature,
+  generateSiweNonce,
+  MAX_SIWE_MESSAGE_LENGTH,
+} from './siwe.helpers';
+
+export { InMemoryNonceStore } from './nonceStore';
+export type { NonceStore } from './nonceStore';
+
+export { verifySiweSignatureWithReplayProtection } from './replayProtection';
